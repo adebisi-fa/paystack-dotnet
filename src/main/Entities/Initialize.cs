4ebdf780817473859f3d5efd7cf28be48ef35f46
adebisi-fa-hp <adebisi-fa@live.com>
2016-12-19 06:26:03 +0100

    Refactor: Separate API entities.

---
+++ src/main/Entities/Initialize.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace PayStack.Net
+{
+    public class InitializeRequest
+    {
+        public string Reference { get; set; }
+
+        [JsonProperty("amount")]
+        public string AmountInKobo { get; set; }
+
+        public string Email { get; set; }
+        public string Plan { get; set; }
+
+        [JsonProperty("callback_url")]
+        public string CallbackUrl { get; set; }
+
+        public string SubAccount { get; set; }
+
+        [JsonProperty("transaction_charge")]
+        public int TransactionCharge { get; set; }
+
+        public string Bearer { get; set; }
+    }
+
+    public class InitializeResponse
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public InitializeResponseData Data { get; set; }
+    }
+
+    public class InitializeResponseData
+    {
+        [JsonProperty("authorization_url")]
+        public string AuthorizationUrl { get; set; }
+
+        [JsonProperty("access_code")]
+        public string AccessCode { get; set; }
+
+        public string Reference { get; set; }
+    }
+}
---
+++ src/main/PayStackApi.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Net;
+using System.Net.Http;
+using XtremeIT.Library.Pins;
+
+namespace PayStack.Net
+{
+    public class PayStackApi : IPayStackApi
+    {
+
+        private readonly HttpClient _client;
+        private readonly JsonSerializerSettings _jsonSerializerSettings;
+
+        public PayStackApi(string secretKey)
+        {
+            _jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+               
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            _client = new HttpClient {BaseAddress = new Uri("https://api.paystack.co/")};
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", secretKey);
+            _client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public InitializeResponse Initialize (InitializeRequest request)
+        {
+            request.Reference = $"{request.Reference};{Generator.NewPin(new GeneratorSettings { Domain = GeneratorCharacterDomains.AlphaNumerics, PinLength = 7 })}";
+            return Post<InitializeResponse, InitializeRequest>("transaction/initialize", request);
+        }
+
+        public VerifyResponse Verify(string reference)
+        {
+            return Get<VerifyResponse>($"transaction/verify/{reference}");
+        }
+
+        TR Post<TR, T>(string relativeUrl, T request)
+        {
+            var requestBody = JsonConvert.SerializeObject(request, Formatting.Indented, _jsonSerializerSettings);
+            return JsonConvert.DeserializeObject<TR>(
+                _client.PostAsync(
+                    relativeUrl,
+                    new StringContent(requestBody)
+                ).Result.Content.ReadAsStringAsync().Result
+            );
+        }
+
+        TR Get<TR>(string relativeUrl)
+        {
+            return JsonConvert.DeserializeObject<TR>(
+                _client.GetAsync(relativeUrl).Result.Content.ReadAsStringAsync().Result
+            );
+        }
+    }
+}
